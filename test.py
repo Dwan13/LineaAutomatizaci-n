06dc059cf168273649c3481a9e4d2c8646104f02
Dwan Veloza <dwanveloza@pop-os.localdomain>
2019-05-24 13:17:21 -0500

    algunas soluciones

--- test.py
+++ test.py
@@ -1,18 +1,19 @@
 import unittest
+import sys
+sys.path.insert(0, "PruebaFib")
 
-from PruebaFib import fib
+import fibonacci
 
 class pruebaFibonacci(unittest.TestCase):
-
     def SerieFibonacci_imp(self):
         """ Prueba con n impar """
-        data = [5]
+        data = 5
         result = fib(data)
         self.assertEqual(result,5, "Incorrecta serie de fibonacci con n impar")
 
     def SerieFibonacci_par(self):
         """ Prueba con n par """
-        data = [8]
+        data = 8
         result = fib(data)
         self.assertEqual(result,21, "Incorrecta serie de fibonacci con n par")
   
